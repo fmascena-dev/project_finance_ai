cbb14f5b065c10409fab840d487a515cc04eb680
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-11-30 17:07:54 -0300

    chore: update next version and add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+'use client'
+
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
+  const message = "Welcome to Finance AI!";
+
   return (
-    <h1 className="p-5 text-3xl font-bold text-red-500 underline">Home</h1>
+    <div>
+      <h1 className="p-5 text-3xl font-bold text-red-500 underline">Home</h1>
+      <Button variant="default" onClick={() => alert(message)}>
+        Primary Button
+      </Button>
+    </div>
   );
 }
