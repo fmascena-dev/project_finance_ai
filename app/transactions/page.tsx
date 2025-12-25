705247ffe4928ea311914667dfedb2f61d7a387b
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-12-25 19:46:28 -0300

    feat: ensure transaction table labels are rendered property

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -13,7 +13,11 @@ export default async function TransactionsPage() {
     <div className="space-y-6 p-6">
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <Button variant="outlineTwo" size="smTwo" className="rounded-full">
+        <Button
+          variant="outlineTwo"
+          size="smTwo"
+          className="rounded-full font-bold"
+        >
           Adicionar Transação
           <ArrowDownUpIcon />
         </Button>
