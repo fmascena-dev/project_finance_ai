eb210e32f56194a44c6f023a0c66b1778d62569f
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-12-26 00:34:30 -0300

    feat: create transaction dialog

---
+++ app/_components/add-transaction-button.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import UpsertTransactionDialog from "./upsert-transaction-dialog";
+
+const AddTransactionButton = () => {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+
+  return (
+    <>
+      <Button
+        variant="outlineTwo"
+        className="rounded-full font-bold"
+        onClick={() => setDialogIsOpen(true)}
+      >
+        Adicionar transação
+        <ArrowDownUpIcon />
+      </Button>
+      <UpsertTransactionDialog
+        isOpen={dialogIsOpen}
+        setIsOpen={setDialogIsOpen}
+      />
+    </>
+  );
+};
+
+export default AddTransactionButton;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,8 +1,7 @@
-import { ArrowDownUpIcon } from "lucide-react";
-import { Button } from "../_components/ui/button";
 import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
+import AddTransactionButton from "../_components/add-transaction-button";
 
 export default async function TransactionsPage() {
   // acessar as transações do banco de dados
@@ -13,14 +12,7 @@ export default async function TransactionsPage() {
     <div className="space-y-6 p-6">
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <Button
-          variant="outlineTwo"
-          size="smTwo"
-          className="rounded-full font-bold"
-        >
-          Adicionar Transação
-          <ArrowDownUpIcon />
-        </Button>
+        <AddTransactionButton />
       </div>
       <DataTable columns={transactionColumns} data={transactions} />
     </div>
