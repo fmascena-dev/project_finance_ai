6c4873ad9c1d113969d2a2801d30c5003d20dcbd
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-12-27 01:57:32 -0300

    feat: ensure transactions and subscriptions pages are protected and update styles badge

--- app/transactions/_components/type-badge.tsx
+++ app/transactions/_components/type-badge.tsx
@@ -9,23 +9,23 @@ interface TransactionTypeBadgeProps {
 const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
   if (transaction.type === TransactionType.DEPOSIT) {
     return (
-      <Badge className="flex w-28 select-none items-center justify-center bg-deposit text-sm font-bold text-primary hover:bg-deposit">
-        <CircleIcon className="mr-2 fill-primary" size={10} />
+      <Badge className="select-none bg-deposit font-bold text-primary hover:bg-deposit">
+        <CircleIcon className="mr-2 shrink-0 fill-primary" size={10} />
         Depósito
       </Badge>
     );
   }
   if (transaction.type === TransactionType.EXPENSE) {
     return (
-      <Badge className="flex w-28 select-none items-center justify-center bg-expense text-sm font-bold text-danger hover:bg-expense">
-        <CircleIcon className="mr-2 fill-danger" size={10} />
+      <Badge className="select-none bg-expense font-bold text-danger hover:bg-expense">
+        <CircleIcon className="mr-2 shrink-0 fill-danger" size={10} />
         Despesa
       </Badge>
     );
   }
   return (
-    <Badge className="flex w-32 select-none items-center justify-center bg-investment text-sm font-bold text-muted-two hover:bg-investment">
-      <CircleIcon className="mr-2 fill-white" size={10} />
+    <Badge className="select-none bg-investment font-bold text-muted-two hover:bg-investment">
+      <CircleIcon className="mr-2 shrink-0 fill-muted-two" size={10} />
       Investimento
     </Badge>
   );
