212e182e2f66b515362983debf91cc6865667680
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-12-28 02:05:25 -0300

    feat: add last transactions component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,36 +1,55 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import NavBar from "../_components/navbar";
+import Navbar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
 import TimeSelect from "./_components/time-select";
 import { isMatch } from "date-fns";
+import TransactionsPieChart from "./_components/transactions-pie-chart";
+import { getDashboard } from "../_data/get-dashboard";
+import LastTransactions from "./_components/last-transactions";
+import ExpensesPerCategory from "./_components/expenses-per-category";
 
 interface HomeProps {
   searchParams: {
     month: string;
+    year?: string;
   };
 }
 
-export default function Home({ searchParams: { month } }: HomeProps) {
-  const { userId } = auth();
+export default async function Home({
+  searchParams: { month, year },
+}: HomeProps) {
+  const { userId } = await auth();
   if (!userId) {
     redirect("/login");
   }
-
   const monthIsInvalid = !month || !isMatch(month, "MM");
   if (monthIsInvalid) {
-    redirect("?month=01");
+    redirect(
+      `?month=${new Date().getMonth() + 1}&year=${new Date().getFullYear()}`,
+    );
   }
-
+  const dashboard = await getDashboard(month, year);
   return (
     <>
-      <NavBar />
-      <div className="space-y-6 p-6">
+      <Navbar />
+      <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <SummaryCards month={month} />
+        <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
+          <div className="flex flex-col gap-6 overflow-hidden">
+            <SummaryCards {...dashboard} />
+            <div className="grid h-full grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
+              <TransactionsPieChart {...dashboard} />
+              <ExpensesPerCategory
+                expensesPerCategory={dashboard.totalExpensePerCategory}
+              />
+            </div>
+          </div>
+          <LastTransactions lastTransactions={dashboard.lastTransactions} />
+        </div>
       </div>
     </>
   );
